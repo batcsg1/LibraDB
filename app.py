81f321fd608568caccfae5e58044da68a54b63ba
SamBat2005 <batcsg1@student.op.ac.nz>
2026-02-18 20:29:52 +1300

    feat(app.py): setup CLI arg parser and added arg for db name

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
+# Use Python's built-in argparse library for setting up LibraDB CLI
+import argparse
 # Import database engine
 from engine import LibraDB
 
+# Create CLI parser
+parser = argparse.ArgumentParser(description="LibraDB CLI")
+
+# Add argument for creating database file
+parser.add_argument("--db", default="db", help="Name of database file")
+
+# Parse the arguments
+args = parser.parse_args()
+
+## -- Pass the arguments to LibraDB
+
+# Initialize the database engine with the parsed argument
+db = LibraDB(f"{args.db}.toon")
+
+print(f"Connected to {args.db}")
+
 # Initialize the database
-db = LibraDB("my_database.toon")
+#db = LibraDB("my_database.toon")
 
 # Initialize a collection
-users = db.collection("users")
+#users = db.collection("users")
 
 # Insert a user
 # newUser = users.insert({ "name": "Brent", "age": 21 })
 
 # Find all users
-allUsers = users.find({ "query": {"age": {"$gte": 30}}})
+#allUsers = users.find({ "query": {"age": {"$gte": 30}}})
 
 # Find specific users
 
@@ -21,5 +39,4 @@ allUsers = users.find({ "query": {"age": {"$gte": 30}}})
 
 # Show a specific amount of fields of the user object
 
-
-print(allUsers)
+#print(allUsers)
