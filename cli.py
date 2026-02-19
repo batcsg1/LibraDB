b56dfd8ff57656dc72a4805d31904650e8e0c6ab
SamBat2005 <batcsg1@student.op.ac.nz>
2026-02-19 19:51:23 +1300

    refactor(cli): improved help for arguments

--- cli.py
+++ cli.py
@@ -4,10 +4,10 @@ import argparse
 class LibraCLI:
     def __init__(self):
         # Setting up parser
-        self.parser = argparse.ArgumentParser(description="LibraDB v1.0.0 CLI")
+        self.parser = argparse.ArgumentParser(description="LibraDB v1.0.0 - A simple no BS serverless No-SQL database management system")
         # Argument for specifying database name
-        self.parser.add_argument("--db", default="db", help="Name of database file")
-        self.parser.add_argument("--collection", "-c", help="Setup a data collection" )
+        self.parser.add_argument("--db", default="db", help="Name of database file you want to load or create")
+        self.parser.add_argument("--collection", "-c", help="Name of data collection within the database")
         self.parser.add_argument(
             "--find",
             "-f", 
